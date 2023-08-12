4b99301ac4010fce2d130b27d742006aaf5a192c
yeongbin <yeongbin6483@gmail.com>
2023-08-12 18:16:11 +0900

    06. 회원가입과 로그인 기능 개발하기(2) 까지 커밋 UserControllerTest, UserServiceTest 전부 정상 수행 확인

---
+++ src/main/java/com/example/sns/controller/request/UserJoinRequest.java
@@ -0,0 +1,13 @@
+package com.example.sns.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserJoinRequest {
+    private String userName;
+    private String password;
+}
